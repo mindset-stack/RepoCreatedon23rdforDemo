8bf4ef755a5647e1763f285ce0bbceca3c5856c3
vijay.sankpal <vijay.sankpal@blazelcan.com>
2022-12-02 17:20:08 +0530

    Shadow closed Element

--- src/test/java/Packageone/SelectorHub.java
+++ src/test/java/Packageone/SelectorHub.java
@@ -9,13 +9,18 @@ import org.openqa.selenium.interactions.Actions;
 
 public class SelectorHub {
 
-    public static void main(String []args){
+    public static void main(String []args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "\\D:\\chromedriver.exe");
         WebDriver driver= new ChromeDriver();
-        driver.get("https://selectorshub.com/xpath-practice-page/");
+        driver.get("https://selectorshub.com");
 //        driver.switchTo().frame("shadpact");
 //        JavascriptExecutor jse=(JavascriptExecutor)driver;
-        driver.findElement(By.xpath("//div[@id='userPass']"));
+//        jse.executeScript( "return document.querySelector("#userName").shadowRoot.querySelector("#kils"));
+
+        driver.findElement(By.xpath("//*[@id=\"menu-item-4098\"]/a")) ;
+        Thread.sleep(10000);
+        String test=driver.findElement(By.xpath("//div[@id='jest']")).getText();
+        System.out.println(test);
         Actions act= new Actions(driver);
         act.sendKeys(Keys.TAB).perform();
         act.sendKeys("1234");
